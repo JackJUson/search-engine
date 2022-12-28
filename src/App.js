d4201f0792341be8112806f389a14175c395c6ba
Jackson <z5397791@ad.unsw.edu.au>
2022-12-28 23:16:51 +1100

    Add home page jsx file and page route in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
+    // BEM
     <div className="App">
       <h1>Search Engine 🔎</h1>
+
+      {/* Home page (Search bar) */}
+      <Home></Home>
+
+      {/* Results page */}
     </div>
   );
 }
