1ae45f814d89778e8667492de5e3e44312ad8344
Jackson <z5397791@ad.unsw.edu.au>
2022-12-29 21:07:31 +1100

    Fixed typo in google search api fetch

--- src/useGoogleSearch.jsx
+++ src/useGoogleSearch.jsx
@@ -3,8 +3,8 @@ import API_KEY from "./Key.jsx";
 
 const CONTEXT_KEY = "663178f46916240f2";
 
+// Custom hook to use Google Search
 function useGoogleSearch(term) {
-  // Custom hook
   const [data, setData] = useState(null);
 
   useEffect(() => {
@@ -12,8 +12,16 @@ function useGoogleSearch(term) {
       fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
       )
+      .then(response => response.json())
+      .then(result => {
+        setData(result)
+      })
     }
+
+    fetchData();
   }, [term]);
-}
+
+  return { data }
+};
 
 export default useGoogleSearch;
