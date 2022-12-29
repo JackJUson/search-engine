fe402ecdc89263cb454bb52745945523e0137bf8
Jackson <z5397791@ad.unsw.edu.au>
2022-12-29 11:44:28 +1100

    Add google logo image to home body div in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import Avatar from "@mui/material/Avatar";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <h1>Lets do this!!</h1>
-
       <div className="home__header">
         <div className="home__header--left">
           <Link to="/about">About</Link>
@@ -15,11 +15,19 @@ function Home() {
         <div className="home__header--right">
           <Link to="/gmail">Gmail</Link>
           <Link to="/images">Images</Link>
-          {/* Icon */}
-          {/* Avatar */}
+          <AppsIcon className="home__header--apps" />
+          <Avatar />
         </div>
       </div>
-      <div className="home__body"></div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt=""
+        />
+        <div className="home__input--container">
+          {/* <Search /> */}
+        </div>
+      </div>
     </div>
   );
 }
